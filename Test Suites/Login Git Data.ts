f68d69ebd6395d6c04c30b2c9924b897558d4b50
krixwu <92783689+krixwu@users.noreply.github.com>
2024-03-26 23:09:18 +0700

    push test case git data

---
+++ Test Suites/Login Git Data.ts
@@ -0,0 +1,48 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Login Git Data</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>779045ab-05c2-4903-8499-24099a2c86e9</testSuiteGuid>
+   <testCaseLink>
+      <guid>a0b264a3-76f8-4af4-aceb-9066fe41122b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login Git Test</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>f2186ea7-74bb-409b-9412-08c4cc632444</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/Login Git Test</testDataId>
+      </testDataLink>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId>f2186ea7-74bb-409b-9412-08c4cc632444</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>username</value>
+         <variableId>a9a78771-4ccd-48ef-8563-49221efadf1b</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>f2186ea7-74bb-409b-9412-08c4cc632444</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>password</value>
+         <variableId>40b1a138-0e38-4d6d-9fe8-4b157e2a8c2d</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>f2186ea7-74bb-409b-9412-08c4cc632444</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>expected_msg</value>
+         <variableId>cc67c483-0234-4bf1-80b1-8804581d9e54</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
